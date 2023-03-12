0e17d93dbd4ed8136214e685e272e42b2b568f62
Mohamed-Taher1 <mohamedtahermail@gmail.com>
2023-03-12 21:46:40 +0200

    first commit

---
+++ lib/features/presentation/views/widgets/spalash_view_body.dart
@@ -0,0 +1,17 @@
+import 'package:bookly_app/core/utils/assets.dart';
+import 'package:flutter/material.dart';
+
+class SplashViewBody extends StatelessWidget {
+  const SplashViewBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        Image.asset(AssetsData.logo),
+      ],
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:bookly_app/constants.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get_navigation/src/root/get_material_app.dart';
+
+import 'features/presentation/views/splash_view.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData().copyWith(
+        scaffoldBackgroundColor: primaryColor,
+      ),
+      home: const SplashView(),
+    );
+  }
+}
